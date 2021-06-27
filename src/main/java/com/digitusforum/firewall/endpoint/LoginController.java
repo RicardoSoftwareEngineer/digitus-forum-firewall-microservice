8b2c2c93c60d054256e25767df7380d2da096a73
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-27 10:04:31 -0300

    cleanup

--- src/main/java/com/digitusforum/firewall/endpoint/LoginController.java
+++ src/main/java/com/digitusforum/firewall/endpoint/LoginController.java
@@ -1,9 +1,5 @@
 package com.digitusforum.firewall.endpoint;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.digitusforum.firewall.service.LoginMicroservice;
-import com.digitusforum.firewall.service.UserService;
 
 import model.Headers;
 import model.M;
@@ -24,17 +19,14 @@ import vo.UserVO;
 @CrossOrigin(origins = "*")
 @RestController
 public class LoginController {
-	@Autowired
-    UserService userService;
-	//UserService userService = new UserService();
-    //Map<String, TokenVO> tokenCache = new HashMap<>(); //lets keep this to the future
 
-    @PostMapping(value = "/firewall/v1/login/byEmailAndPassword")
-    public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale, @RequestBody UserVO userVO) {
-    	 if (!RequestService.isUp(Microservices.LOGIN)){
-    		 throw ThrowService.doIt(locale, 503, M.LOGIN_MICROSERVICE_OFFLINE);   
-        }
-    	 TokenVO token = new LoginMicroservice().loginWithEmailAndPassword(userVO, Headers.DEFAULT(locale.toString()));
-        return token;
-    }
+	@PostMapping(value = "/firewall/v1/login/byEmailAndPassword")
+	public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale,
+			@RequestBody UserVO userVO) {
+		if (!RequestService.isUp(Microservices.LOGIN)) {
+			throw ThrowService.doIt(locale, 503, M.LOGIN_MICROSERVICE_OFFLINE);
+		}
+		TokenVO token = new LoginMicroservice().loginWithEmailAndPassword(userVO, Headers.DEFAULT(locale.toString()));
+		return token;
+	}
 }
--- src/main/java/com/digitusforum/firewall/endpoint/UserController.java
+++ src/main/java/com/digitusforum/firewall/endpoint/UserController.java
@@ -1,60 +1,60 @@
 package com.digitusforum.firewall.endpoint;
 
-import com.digitusforum.firewall.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import vo.UserVO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import vo.UserVO;
 
 @CrossOrigin(origins = "*")
 @RestController
-public class UserController  {
-    @Autowired
-    UserService userService;
-    //todo user´s crud
-    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
-    public ResponseEntity handle() {
-        return new ResponseEntity(HttpStatus.OK);
-    }
+public class UserController {
+	@RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
+	public ResponseEntity handle() {
+		return new ResponseEntity(HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "/user/create")
-    public Object create() {
-        return "";
-    }
+	@RequestMapping(value = "/user/create")
+	public Object create() {
+		return "";
+	}
 
-    @RequestMapping(value = "/user/retrieve")
-    public Object retrieve(@PathVariable Optional<String> id) {
-        return "hi dad";
-    }
+	@RequestMapping(value = "/user/retrieve")
+	public Object retrieve(@PathVariable Optional<String> id) {
+		return "hi dad";
+	}
 
-    @RequestMapping(value = "/user/{id}/retrieve")
-    public Object retrieve() {
-        return "hi dad";
-    }
+	@RequestMapping(value = "/user/{id}/retrieve")
+	public Object retrieve() {
+		return "hi dad";
+	}
 
-    @RequestMapping(value = "/user/{id}/update")
-    public Object update() {
-        return "hi dad";
-    }
+	@RequestMapping(value = "/user/{id}/update")
+	public Object update() {
+		return "hi dad";
+	}
 
-    @RequestMapping(value = "/user/{id}/delete")
-    public Object delete() {
-        return "hi dad";
-    }
+	@RequestMapping(value = "/user/{id}/delete")
+	public Object delete() {
+		return "hi dad";
+	}
 
-    @RequestMapping(value = "/user/{id}/validate")
-    public Object validate() {
-        return "hi dad";
-    }
+	@RequestMapping(value = "/user/{id}/validate")
+	public Object validate() {
+		return "hi dad";
+	}
 
-    @RequestMapping(value = "/user/login")
-    public Object create(@RequestHeader(defaultValue = "en_us") String locale,
-                         @RequestBody UserVO user) {
-        return null;// userService.loginWithEmailAndPassword(user, locale);
-    }
-
+	@RequestMapping(value = "/user/login")
+	public Object create(@RequestHeader(defaultValue = "en_us") String locale, @RequestBody UserVO user) {
+		return null;// userService.loginWithEmailAndPassword(user, locale);
+	}
 
 }
