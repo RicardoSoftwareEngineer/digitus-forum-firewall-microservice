854d1bc43688cb30575b1d5995ab338054c1a9ae
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-27 00:52:47 -0300

    endpoint login/v1/loginByEmailAndPassword now can bring user information

--- src/main/java/com/digitusforum/firewall/endpoint/LoginController.java
+++ src/main/java/com/digitusforum/firewall/endpoint/LoginController.java
@@ -27,27 +27,14 @@ public class LoginController {
 	@Autowired
     UserService userService;
 	//UserService userService = new UserService();
-    Map<String, TokenVO> tokenCache = new HashMap<>();
+    //Map<String, TokenVO> tokenCache = new HashMap<>(); //lets keep this to the future
 
-    @PostMapping(value = "/firewall/v1/login/by/emailAndPassword")
+    @PostMapping(value = "/firewall/v1/login/byEmailAndPassword")
     public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale, @RequestBody UserVO userVO) {
-    	if(tokenCache.containsKey(userVO.getEmail()+userVO.getPassword())){
-            new Thread(() -> {
-                TokenVO token = new LoginMicroservice().loginWithEmailAndPassword(userVO, Headers.DEFAULT(locale.toString()));
-                tokenCache.put(userVO.getEmail()+userVO.getPassword(), token);
-            }).start();
-        }else if (RequestService.isUp(Microservices.LOGIN)){
-            TokenVO token = new LoginMicroservice().loginWithEmailAndPassword(userVO, Headers.DEFAULT(locale.toString()));
-            tokenCache.put(userVO.getEmail()+userVO.getPassword(), token);
-        }else{
-            throw ThrowService.doIt(locale, 503, M.LOGIN_MICROSERVICE_OFFLINE);
+    	 if (!RequestService.isUp(Microservices.LOGIN)){
+    		 throw ThrowService.doIt(locale, 503, M.LOGIN_MICROSERVICE_OFFLINE);   
         }
-        return tokenCache.get(userVO.getEmail()+userVO.getPassword());
-    }
-
-    @PostMapping(value = "/firewall/v1/login/eraseCache/tokenCache")
-    public Object eraseTokenCache(@RequestBody UserVO userVO) {
-    	tokenCache.remove(userVO.getEmail()+userVO.getPassword());
-    	return "cache erased";
+    	 TokenVO token = new LoginMicroservice().loginWithEmailAndPassword(userVO, Headers.DEFAULT(locale.toString()));
+        return token;
     }
 }
