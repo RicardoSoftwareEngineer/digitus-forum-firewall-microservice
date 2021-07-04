2e4d8ca59520eae6961647caff49d7351a0bab42
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-04 18:27:19 -0300

    dependecy injection added for a more decoupled environment

--- src/main/java/com/digitusforum/firewall/endpoint/FirewallUserController.java
+++ src/main/java/com/digitusforum/firewall/endpoint/FirewallUserController.java
@@ -2,8 +2,6 @@ package com.digitusforum.firewall.endpoint;
 
 import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,19 +9,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.digitusforum.firewall.service.LoginMicroservice;
 
 import microservice.UserMicroservice;
+import service.RequestService;
 import vo.UserVO;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class FirewallUserController {
-	LoginMicroservice loginMicroservice = new LoginMicroservice();
-	UserMicroservice userMicroservice = new UserMicroservice();
+	LoginMicroservice loginMicroservice = new LoginMicroservice(new RequestService());
+	UserMicroservice userMicroservice = new UserMicroservice(new RequestService());
 
 	@PostMapping(value = "/firewall/user/v1/create")
 	public Object create(@RequestHeader(defaultValue = "en_us") String locale, @RequestHeader String Authorization,
