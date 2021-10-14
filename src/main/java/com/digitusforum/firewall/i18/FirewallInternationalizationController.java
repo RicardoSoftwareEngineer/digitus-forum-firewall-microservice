0892f21017734214ff8beec5375c9880739790d7
Ricardo Ribeiro <riceirolves@gmail.com>
2021-10-13 23:35:24 -0300

    me

--- src/main/java/com/digitusforum/firewall/i18/FirewallInternationalizationController.java
+++ src/main/java/com/digitusforum/firewall/i18/FirewallInternationalizationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import i18.I18Microservice;
-import i18.InternationalizationVO;
+import i18.I18VO;
 import request.RequestServiceDEPRECATED;
 
 //todo make this guy in kotlin
@@ -17,7 +17,7 @@ public class FirewallInternationalizationController {
 	I18Microservice i18Microservice = new I18Microservice(new RequestServiceDEPRECATED());
 
 	@PostMapping(value = "/firewall/internationalization/v1/i18")
-	public Object internationalization(@RequestBody Optional<InternationalizationVO> i18) {
+	public Object internationalization(@RequestBody Optional<I18VO> i18) {
 		return i18Microservice.getMessageByKey(i18.get());
 	}
 	
